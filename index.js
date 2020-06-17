731878a0f58d24cfce2a39dce08063776b656c86
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-06-17 15:13:58 +0100

    formatting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // helpful DOM bits
-const stack = document.querySelector(".portfolio__tech-stack")
+const stack = document.querySelector(".portfolio__tech-stack");
 const project0 = document.getElementById("project0");
 const projectsFrame = document.querySelector(".portfolio__projects-frame");
 const stackFocus = document.getElementsByClassName("stack--focus");
@@ -16,7 +16,7 @@ const techNames = [
   jest, //5
   postgres, //6
   node, //7
-  restful, //8
+  restful, //8 
   , //9 - removed express
   netlify, //10
   eleventy, //11
@@ -32,23 +32,76 @@ const techNames = [
 ];
 
 const projectStacks = [
-  [ //WIP
-    4,5,6,7,8,10,12,14,15,16,17,18,20,
+  [
+    //WIP
+    4,
+    5,
+    6,
+    7,
+    8,
+    10,
+    12,
+    14,
+    15,
+    16,
+    17,
+    18,
+    20,
   ],
-  [ //CAF
-    4,5,8,10,14,15,16,17,18,19,20,
+  [
+    //CAF
+    4,
+    5,
+    8,
+    10,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
   ],
-  [ //SRVVRS
-    0,1,3,6,7,8,12,13,15,18,
+  [
+    //SRVVRS
+    0,
+    1,
+    3,
+    6,
+    7,
+    8,
+    12,
+    13,
+    15,
+    18,
   ],
-  [ //Snakes
-    1,4,5,8,10,15,18,
+  [
+    //Snakes
+    1,
+    4,
+    5,
+    8,
+    10,
+    15,
+    18,
   ],
-  [ //Journalist
-    0,2,3,10,11,18,
+  [
+    //Journalist
+    0,
+    2,
+    3,
+    10,
+    11,
+    18,
   ],
-  [ //Password
-    0,1,3,6,7,8,
+  [
+    //Password
+    0,
+    1,
+    3,
+    6,
+    7,
+    8,
   ],
 ];
 
@@ -62,19 +115,19 @@ const setFrame = () => {
 
 const setStack = (techRefs) => {
   // if passed a project , focus the relevant tech otherwise focus all of them
-  if (techRefs){
-    techRefs.forEach(ref => {
+  if (techRefs) {
+    techRefs.forEach((ref) => {
       document.getElementById(techNames[ref].id).classList.add("stack--focus");
-    })
+    });
   } else {
-    techNames.forEach(name => {
-      document.getElementById(name.id).classList.add("stack--focus");      
-    })
+    techNames.forEach((name) => {
+      document.getElementById(name.id).classList.add("stack--focus");
+    });
   }
 };
 
 const updateStack = () => {
-  if (projectsTop-stackTop < 300){
+  if (projectsTop - stackTop < 300) {
     project0PosX = project0.getBoundingClientRect().left;
     // as project 0 moves left, find how far it has moved in (absolute) multiples of its own width
     projectNum = Math.abs((project0PosX - projectsEdge) / projectWidth);
@@ -86,10 +139,10 @@ const updateStack = () => {
       } // focus stack for incoming project
       setStack(projectStacks[projectNum]);
     }
-  } else if (stackFocus.length<techNames.length) {
-      setStack(); // focus all icons if wrapped
-    }
+  } else if (stackFocus.length < techNames.length) {
+    setStack(); // focus all icons if wrapped
   }
+};
 
 // initialise window
 setFrame();
@@ -102,7 +155,7 @@ projectsFrame.addEventListener("scroll", updateStack);
 
 mobileFrame.addEventListener("click", () => {
   projectsFrame.focus();
-})
+});
 
 // form validation
 const form = document.querySelector("form");
@@ -112,7 +165,7 @@ const nameInput = form.querySelector("#name");
 const nameRegex = /^[a-zA-Z-.' ]{2,}$/;
 
 const emailInput = form.querySelector("#email");
-const emailRegex = (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const messageInput = form.querySelector("#message");
 const messageRegex = /^(?!\s*$).+/;
@@ -120,20 +173,20 @@ const messageRegex = /^(?!\s*$).+/;
 const thanks = document.querySelector(".form__thanks");
 
 let valid = {
-  name: false, 
-  email: false, 
-  message: false, 
-}
+  name: false,
+  email: false,
+  message: false,
+};
 
 const errorMessage = {
   name: "Check your name doesn't contain odd characters.",
   email: "This needs to include an @ symbol and domain.",
-  message: "Please include some detail of why you are reaching out."
-}
+  message: "Please include some detail of why you are reaching out.",
+};
 
 form.setAttribute("novalidate", "");
 
-form.addEventListener("submit", event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   if (Object.values(valid).includes(false)) {
     for (key in valid) {
@@ -141,40 +194,51 @@ form.addEventListener("submit", event => {
         input = document.getElementById(key);
         input.style.borderColor = "hsl(0, 100%, 45%)";
         input.nextElementSibling.textContent = errorMessage[key];
-        input.nextElementSibling.style.visibility = 'initial';
-        input.nextElementSibling.style.opacity = '1';
-        input.nextElementSibling.setAttribute("role", "alert")
+        input.nextElementSibling.style.visibility = "initial";
+        input.nextElementSibling.style.opacity = "1";
+        input.nextElementSibling.setAttribute("role", "alert");
       }
     }
   } else {
     fetch("/", {
       method: "POST",
-      headers: {"Content-Type": "application/x-www-form-urlencoded"},
-      body: new URLSearchParams("form-name=contact&" + 
-                      nameInput.name + "=" + nameInput.value + "&" + 
-                      emailInput.name + "="  + emailInput.value + "&" + 
-                      messageInput.name + "="  + messageInput.value)
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(
+        "form-name=contact&" +
+          nameInput.name +
+          "=" +
+          nameInput.value +
+          "&" +
+          emailInput.name +
+          "=" +
+          emailInput.value +
+          "&" +
+          messageInput.name +
+          "=" +
+          messageInput.value
+      ),
     })
-    .then(()=>{ // need to refactor!
-      nameInput.style.border = ".2rem solid hsl(223, 55%, 22%)";
-      nameInput.value="";
-      emailInput.style.border = ".2rem solid hsl(223, 55%, 22%)";
-      emailInput.value="";
-      messageInput.style.border = ".2rem solid hsl(223, 55%, 22%)";
-      messageInput.value="";
-      thanks.style.visibility = 'initial';
-      thanks.style.opacity = '1';
-    })
-    .catch(console.error);
+      .then(() => {
+        // need to refactor!
+        nameInput.style.border = ".2rem solid hsl(223, 55%, 22%)";
+        nameInput.value = "";
+        emailInput.style.border = ".2rem solid hsl(223, 55%, 22%)";
+        emailInput.value = "";
+        messageInput.style.border = ".2rem solid hsl(223, 55%, 22%)";
+        messageInput.value = "";
+        thanks.style.visibility = "initial";
+        thanks.style.opacity = "1";
+      })
+      .catch(console.error);
   }
 });
 
-function validate(input, test){
-  thanks.style.visibility = 'hidden'; // maybe better in addeventlistener for user focus
-  thanks.style.opacity = '0';
-  input.nextElementSibling.textContent = '-';
-  input.nextElementSibling.style.visibility = 'hidden';
-  input.nextElementSibling.style.opacity = '0';
+function validate(input, test) {
+  thanks.style.visibility = "hidden"; // maybe better in addeventlistener for user focus
+  thanks.style.opacity = "0";
+  input.nextElementSibling.textContent = "-";
+  input.nextElementSibling.style.visibility = "hidden";
+  input.nextElementSibling.style.opacity = "0";
   input.nextElementSibling.removeAttribute("role");
   if (test) {
     input.style.borderColor = "hsl(106, 100%, 30%)";
